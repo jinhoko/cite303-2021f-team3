e8924d66fc46dc0d1c3cc72654e2e3d67314bdfd
Jinho Ko <jinho.ko@postech.ac.kr>
2021-12-19 15:56:28 +0900

    add snow

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ pygame.init()
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 GRAY1 = (100,100,100)
-image = pygame.image.load(r'images/tree1.png')
-image = pygame.transform.scale(image, (400,500))    # 4: 5 scale
+image = pygame.image.load(r'images/tree2.png')
+image = pygame.transform.scale(image, (750,1000))    # 4: 5 scale
+treebase = pygame.image.load(r'images/coffee-bottom.png')
+treebase = pygame.transform.scale(treebase, (100,140))
+treebase = pygame.transform.rotate(treebase, 180)
 cupbase = pygame.image.load(r'images/coffee-bottom.png')
 cupbase = pygame.transform.scale(cupbase, (50,100))
 cupmiddle = pygame.image.load(r'images/coffee-stack.png')
-cupmiddle = pygame.transform.scale(cupmiddle, (50,100))
+cupmiddle = pygame.transform.scale(cupmiddle, (50,100))                    
 starsize = (100,100)
 stars = [ 
     pygame.transform.scale( pygame.image.load(r'images/s1.png'), starsize),
@@ -22,9 +25,22 @@ stars = [
     pygame.transform.scale( pygame.image.load(r'images/s4.png'), starsize),
     pygame.transform.scale( pygame.image.load(r'images/s5.png'), starsize)
 ]
+
+leaf = pygame.image.load('images/leaf.png')
+leaf = pygame.transform.scale(leaf, (40,60)) # 2:3
+
+leaves = [
+    pygame.transform.rotate(leaf, -10),
+    pygame.transform.rotate(leaf, -5),
+    pygame.transform.rotate(leaf, 0),
+    pygame.transform.rotate(leaf, 5),
+    pygame.transform.rotate(leaf, 10),
+]
+
+
 font1 = pygame.font.Font('fonts/notosanskr.ttf', 70) 
 
-text1 = font1.render("텀블러 쓰셈", True, WHITE)
+text1 = font1.render("텀블러 쓰세요", True, WHITE)
 
 
 #screen = pygame.display.set_mode([1600, 1200], pygame.FULLSCREEN) 
@@ -34,38 +50,36 @@ clock = pygame.time.Clock()
 
 target_cnt = 50
 current_cnt = 10 # TODO change this
+ratio = current_cnt / target_cnt
 # make 5 interim steps
 
-def getProgress():
-    if current_cnt < 10:
-        return 1
-    elif current_cnt < 20:
-        return 2
-    elif current_cnt < 30:
-        return 3
-    elif current_cnt < 40:
-        return 4
-    else:
-        return 5
+def getXRange(r):
+    return (100 + 375*r, 850-370*r)
+def getY(r):
+    return 950 - 750*r
+
+numLeaves = 200
+
+
 
 """
 Tree
 """
 riseinterval = 10
 def drawTree():
-    screen.blit(image, (200, 675 - current_cnt*riseinterval)) # center: 400
+    # screen.blit(image, (200, 675 - current_cnt*riseinterval)) # center: 400
     # draw star
-    screen.blit( stars[getProgress() - 1], (400-50, 675 - current_cnt*riseinterval - 50) )
+    screen.blit( stars[0], (425, 200) )
+
+    screen.blit(image, ( 100, 200))
+    screen.blit(treebase, (420,1080))
 
 """
 Cups
 """
 def drawCups():
-    # draw first cup
-    screen.blit(cupbase, (400-25, 1100))
-    # draw others
-    for i in range(current_cnt):
-        screen.blit(cupmiddle, (400-25, 1100 - (i+1)*riseinterval ))
+    
+    screen.blit(leaves[0], (400,800))
 
 """
 Snow
@@ -105,13 +119,13 @@ def drawBottom():
 Text / desc
 """
 def drawDesc():
-    x1 = 100
-    x2 = 700
-    pygame.draw.line(screen, GRAY1, [x1,675 - 10*riseinterval], [x2,675 - 10*riseinterval], 5)
-    pygame.draw.line(screen, GRAY1, [x1,675 - 20*riseinterval], [x2,675 - 20*riseinterval], 5)
-    pygame.draw.line(screen, GRAY1, [x1,675 - 30*riseinterval], [x2,675 - 30*riseinterval], 5)
-    pygame.draw.line(screen, GRAY1, [x1,675 - 40*riseinterval], [x2,675 - 40*riseinterval], 5)
-    pygame.draw.line(screen, GRAY1, [x1,675 - 50*riseinterval], [x2,675 - 50*riseinterval], 5)
+    # x1 = 100
+    # x2 = 700
+    # pygame.draw.line(screen, GRAY1, [x1,675 - 10*riseinterval], [x2,675 - 10*riseinterval], 5)
+    # pygame.draw.line(screen, GRAY1, [x1,675 - 20*riseinterval], [x2,675 - 20*riseinterval], 5)
+    # pygame.draw.line(screen, GRAY1, [x1,675 - 30*riseinterval], [x2,675 - 30*riseinterval], 5)
+    # pygame.draw.line(screen, GRAY1, [x1,675 - 40*riseinterval], [x2,675 - 40*riseinterval], 5)
+    # pygame.draw.line(screen, GRAY1, [x1,675 - 50*riseinterval], [x2,675 - 50*riseinterval], 5)
 
     screen.blit(text1, [1200, 500])
 
@@ -133,10 +147,10 @@ while running:
     screen.fill((0, 0, 0))
 
 	# draw surface
-    #drawDesc()
+    drawDesc()
     #drawBottom()
-    #drawCups()
-    #drawTree()
+    drawTree()
+    drawCups()
     drawSnow()
     
 
